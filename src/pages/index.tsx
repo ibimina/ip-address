8001f95ad465b721d0397c3d58f736489e7b7b57
ibimina <96787210+ibimina@users.noreply.github.com>
2023-02-08 23:45:34 +0100

    docs: added a ip address modal for  invalid ip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,22 @@ import { useState } from 'react'
 import useFetch from '@/useFetch'
 
 const Map = dynamic(
-    () => import('../components/map'), // replace '@components/map' with your component's location
-    { ssr: false } // This line is important. It's what prevents server-side render
-  )
+  () => import('../components/map'), // replace '@components/map' with your component's location
+  { ssr: false } // This line is important. It's what prevents server-side render
+)
 export default function Home() {
   const [ipaddress, setIpaddress] = useState("")
+  const [display, setDisplay] = useState('false')
+  const [err, setErr] = useState("")
   const { details } = useFetch(ipaddress)
 
   const validateIpaddress = (text: string) => {
     let pattern = (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/).test(text)
     if (pattern) {
       setIpaddress(text)
+    } else {
+      setDisplay('true')
+      setErr(text)
     }
   }
   const handleSubmit = (e: React.SyntheticEvent) => {
@@ -23,7 +28,15 @@ export default function Home() {
     const target = e.target as typeof e.target & {
       ipaddress: { value: string };
     };
-    validateIpaddress(target.ipaddress.value)
+    if (target.ipaddress.value.trim()) {
+      validateIpaddress(target.ipaddress.value)
+    }
+
+  }
+  const closeModal = (e: React.MouseEvent) => {
+    setDisplay("false")
+    setErr("")
+    document.querySelector("form")!.reset()
   }
   return (
     <>
@@ -32,7 +45,7 @@ export default function Home() {
         <meta name="description" content="Get location, timezone and isp of any IP Address" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-       
+
       </Head>
       <header className={styles.header}>
         <h1 className={styles.title}>IP Address Tracker</h1>
@@ -41,6 +54,13 @@ export default function Home() {
           <input type="submit" value="" className={styles.submit} />
         </form>
       </header>
+      <div className={styles.modalcon} data-visible={display}>
+        
+        <div className={styles.modal}>
+          <p>Oops!! <span className={styles.bold}>{"'"}{err} {"'"}</span>is not a valid IP Address</p>
+       <button onClick={closeModal} aria-label='close modal' className={styles.close}></button>
+        </div>
+      </div>
       <article className={styles.details}>
         <div className={styles.box}>
           <p className={styles.small}>IP ADDRESS</p>
@@ -61,7 +81,7 @@ export default function Home() {
         <div className={styles.map}>
           {details !== null && <Map lat={details?.location.lat} lng={details?.location.lng} ip={details?.ip} />}
         </div>
-      
+
       </main>
     </>
   )
