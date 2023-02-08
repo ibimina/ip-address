8729a417bcade40ff76d092a9677e7bc31a8b706
ibimina <96787210+ibimina@users.noreply.github.com>
2023-02-08 22:19:17 +0100

    docs: added leaflet stylesheet link and script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+        crossOrigin="" />
+        <Script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
+          integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
+          crossOrigin=""></Script>
+        </Head>
       <body>
         <Main />
         <NextScript />
